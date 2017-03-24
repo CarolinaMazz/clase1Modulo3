1c31b53fe049cce9d06f51ea6a501d8950aeae10
alumno <alumno@mm-15.local>
2017-03-23 19:26:23 -0500

    Initial Commit

---
+++ Multi Tareas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi Tareas
+//
+//  Created by alumno on 3/23/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
