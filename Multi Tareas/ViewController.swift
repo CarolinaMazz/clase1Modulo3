7893bab0dbd1fd547684f346e0ba30069d144b96
alumno <alumno@mm-15.local>
2017-03-23 19:47:43 -0500

    multi tareas con distintos Hilos

--- Multi Tareas/ViewController.swift
+++ Multi Tareas/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var prgProgreso: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnProgreso(_ sender: UIButton) {
+        
+        let hilo = DispatchQueue(label: "pe.ourlimm.tareaPesada")
+        
+        let hilo_2 = DispatchQueue(label: "pe.ourlimm.tareaPesada")
+        
+        hilo.async {
+            for i in 0...100 {
+                print("a \(i)")
+                
+            }
+        }
+        
+        hilo_2.async {
+            for i in 0...100 {
+                print("b \(i)")
+                
+            }
+        }
+        
+        /*
+         hilo.sync {
+            for i in 0...100 {
+                print("a \(i)")
+            }
+         }
+         
+         hilo.async {
+            for i in 0...1000000 {
+                print(i)
+                self.prgProgreso.progress = (Float(i) / 100000.0)
+            }
+        }*/
+        
+        
+        
+    }
 
 }
 
